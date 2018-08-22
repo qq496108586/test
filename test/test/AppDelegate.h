6a8201f2dc20f92184567d925b95cbc29fc7b80a
吴宝发 <wubaofa@szy.cn>
2018-08-22 14:07:58 +0800

    add

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 吴宝发 on 2018/8/22.
+//  Copyright © 2018年 bf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 吴宝发 on 2018/8/22.
+//  Copyright © 2018年 bf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
