c87b43284f6c118238477ee9b29898cf62231347
qq496108586 <496108586@qq.com>
2018-08-22 17:19:05 +0800

    Add files via upload

---
+++ test/test/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  test
+//
+//  Created by 吴宝发 on 2018/8/22.
+//  Copyright © 2018年 bf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"---");
+        
+        
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
